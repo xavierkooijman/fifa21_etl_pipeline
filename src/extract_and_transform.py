d50b5c2ab8228e7422d3876a62ae69330aed435e
xavierkooijman <xavierkooijman@gmail.com>
2026-09-07 14:12:48 +0100

    Extract Contract Ends, Contract Started and Team from Team & Contract column into 3 respective separate columns

--- src/extract_and_transform.py
+++ src/extract_and_transform.py
@@ -14,3 +14,24 @@ df = df.with_columns(
     ((pl.col("Height").str.extract(r"(\d+)", 1).cast(pl.Int32) * 12 +
      pl.col("Height").str.extract(r"'(\d+)", 1).cast(pl.Int32)) * 2.54).round(0).cast(pl.Int32)
 )
+
+
+df = df.with_columns(
+    pl.col("Team & Contract").str.extract(
+        r"~\s*(\d+)", 1).cast(pl.Int32).alias("Contract Ends")
+)
+
+df = df.with_columns(
+    pl.col("Team & Contract").str.extract(r"\s*(\d+)",
+                                          1).cast(pl.Int32).alias("Contract Started")
+)
+
+df = df.with_columns(
+    pl.col("Team & Contract")
+      .str.strip_chars()
+      .str.split("\n")
+      .list.first()
+      .alias("Team")
+)
+
+df = df.drop("Team & Contract")
